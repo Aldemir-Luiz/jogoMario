f2733fb003327bc86abf1d91631581dc3bf3e917
joao06182008-code <joao06182008@gmail.com>
2026-09-17 19:08:23 -0300

    feat:add jump, sprites,

--- frontend/scr/script.js
+++ frontend/scr/script.js
@@ -1,1 +1,18 @@
+const mario = document.querySelector('.mario');
 
+const jump = () => {
+
+  if (mario.classList.contains('jump')) return;
+
+  mario.classList.add('jump');
+
+  setTimeout(() => {
+    mario.classList.remove('jump');
+  }, 700);
+};
+
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'Space') {
+    jump();
+  }
+});
